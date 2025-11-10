0bc27b24a02d68600a15dbc0624a3ca711fa25ce
jhamils1 <jhamil.serrudo@ficct.uagrm.edu.bo>
2025-11-10 13:36:30 -0400

    actualizacion nube

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import LoginForm from "./loginform";
+import LoginForm from "./loginForm";
 import { useAuth } from "../../hooks/useAuth";
 
 const LoginPage = () => {
